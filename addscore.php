df0f528f4e67389ace8d3f5cbea1c107876fa220
Sachin Ravanth <sachin.ravanth@gmail.com>
2018-10-16 12:37:08 +0530

    Initial Commit
    Local files added

---
+++ addscore.php
@@ -0,0 +1,21 @@
+<?php
+	include 'dbconnection.php';
+
+	
+	if (mysqli_connect_error()) {
+		die("Connection failed in addscore...");
+	}
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Update Score | SNS Badminton</title>
+</head>
+<body>
+	<div>
+		<h3>Update Scores</h3>
+		<form>
+		</form>
+	</div>
+</body>
+</html>
---
+++ dbconnection.php
@@ -0,0 +1,7 @@
+<?php
+	$HOST_NAME = "localhost";
+	$DATABASE_NAME = "feedback";
+	$USER_NAME = "root";
+	$PASSWORD = "";
+	$conn = mysqli_connect($HOST_NAME,$USER_NAME,$PASSWORD,$DATABASE_NAME);
+?>
